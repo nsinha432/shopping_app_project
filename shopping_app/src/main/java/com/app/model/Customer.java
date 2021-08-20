68790bdb2746810f8ad7573a792a1a78c79df1ff
nsinha432 <nsinha432@gmail.com>
2021-08-20 13:57:45 +0530

    beta v1.0

--- shopping_app/src/main/java/com/app/model/Customer.java
+++ shopping_app/src/main/java/com/app/model/Customer.java
@@ -42,6 +42,43 @@ public class Customer {
 		this.password = password;
 	}
 	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((fname == null) ? 0 : fname.hashCode());
+		result = prime * result + ((lname == null) ? 0 : lname.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (fname == null) {
+			if (other.fname != null)
+				return false;
+		} else if (!fname.equals(other.fname))
+			return false;
+		if (lname == null) {
+			if (other.lname != null)
+				return false;
+		} else if (!lname.equals(other.lname))
+			return false;
+		return true;
+	}
 	public Customer() {
 		
 	}
