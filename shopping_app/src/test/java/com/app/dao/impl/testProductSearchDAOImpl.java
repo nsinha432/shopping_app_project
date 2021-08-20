87e78a70abd7754c6f279dd65f488e651b6791c7
nsinha432 <nsinha432@gmail.com>
2021-08-20 14:25:29 +0530

    beta v2.0

---
+++ shopping_app/src/test/java/com/app/dao/impl/testProductSearchDAOImpl.java
@@ -0,0 +1,44 @@
+package com.app.dao.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testProductSearchDAOImpl {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetProductsByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProductById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllProducts() {
+		fail("Not yet implemented");
+	}
+
+}
